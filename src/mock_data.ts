0714a65a3ff4babe0d7862c4e25e4e7ccae51f46
ilinieja <b2709f1b@gmail.com>
2023-01-28 12:53:24 +0100

    Add mock data for products and rules

---
+++ src/mock_data.ts
@@ -0,0 +1,36 @@
+import { Product } from "shared/interfaces/product.interface";
+import { Rule } from "shared/interfaces/rule.interface";
+
+export const MOCK_PRODUCTS: Product[] = [
+  { id: "A", price: 50 },
+  { id: "B", price: 30 },
+  { id: "C", price: 20 },
+  { id: "D", price: 15 },
+  { id: "E", price: 50 },
+  { id: "F", price: 30 },
+  { id: "G", price: 20 },
+  { id: "H", price: 15 },
+  { id: "I", price: 50 },
+  { id: "J", price: 30 },
+  { id: "K", price: 20 },
+  { id: "L", price: 15 },
+  { id: "M", price: 50 },
+  { id: "N", price: 30 },
+  { id: "O", price: 20 },
+  { id: "P", price: 15 },
+  { id: "Q", price: 50 },
+  { id: "R", price: 30 },
+  { id: "S", price: 20 },
+  { id: "T", price: 15 },
+  { id: "U", price: 50 },
+  { id: "V", price: 30 },
+  { id: "W", price: 20 },
+  { id: "X", price: 15 },
+  { id: "Y", price: 50 },
+  { id: "Z", price: 30 },
+];
+
+export const MOCK_RULES: Rule[] = [
+  { productId: "A", quantity: 3, price: 130 },
+  { productId: "B", quantity: 2, price: 45 },
+];
--- src/store/rules.slice.ts
+++ src/store/rules.slice.ts
@@ -1,12 +1,22 @@
 import { createEntityAdapter, createSlice } from "@reduxjs/toolkit";
 
-import { Rule } from "../shared/checkout/interfaces/rule.interface";
+import { Rule } from "shared/interfaces/rule.interface";
+import { MOCK_RULES } from "mock_data";
 
-export const rulesAdapter = createEntityAdapter<Rule>();
+export const rulesAdapter = createEntityAdapter<Rule>({
+  selectId: ({ productId }) => productId,
+});
+
+/*
+ * Prepopulating rules with mock data to simplify the initial version.
+ * In real-world scenario they would be received from an API on page init.
+ */
+const initialState = rulesAdapter.getInitialState();
+const prepopulatedState = rulesAdapter.upsertMany(initialState, MOCK_RULES);
 
 export const rulesSlice = createSlice({
   name: "rules",
-  initialState: rulesAdapter.getInitialState(),
+  initialState: prepopulatedState,
   reducers: {
     ruleAdded: rulesAdapter.addOne,
     ruleRemoved: rulesAdapter.removeOne,
